f25a2f208d9d846543793d3447681aa829a85241
LLira34 <luislira1029@gmail.com>
2019-07-12 18:47:36 -0500

    first commit

---
+++ app/src/main/java/net/llira/taskfirebase/MainActivity.java
@@ -0,0 +1,29 @@
+package net.llira.taskfirebase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btnContinue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Inflate
+        btnContinue = findViewById(R.id.btn_continue);
+        // Register listenner
+        btnContinue.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getApplicationContext(), ListTaksActivity.class);
+        startActivity(intent);
+    }
+}//End class
---
+++ app/src/main/java/net/llira/taskfirebase/models/Task.java
@@ -0,0 +1,67 @@
+package net.llira.taskfirebase.models;
+
+public class Task {
+    private String taskId;
+    private String taskKey;
+    private String description;
+    private String priority;
+    private String date;
+    private String status;
+
+    public Task() {
+    }
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskKey() {
+        return taskKey;
+    }
+
+    public void setTaskKey(String taskKey) {
+        this.taskKey = taskKey;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+    @Override
+    public String toString() {
+        return taskKey + '\n' + description + '\n' + priority + '\n' + date + '\n' + status + '\n';
+    }
+}
